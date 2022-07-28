cb691d034e08fbcbc7f8743efd7812224857f34f
Akshay Anil <Akshay.Anil@BRILLIO.COM>
2022-07-28 12:02:17 +0530

    s10 displayed user info on posts

--- src/app/tools/post/post.component.ts
+++ src/app/tools/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PostData } from 'src/app/pages/post-feed/post-feed.component';
+import { FirebaseTSFirestore } from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
 
 @Component({
   selector: 'app-post',
@@ -11,9 +12,29 @@ export class PostComponent implements OnInit {
   //creating ana input to accept he postd oc from post-feed.comp.ts
   @Input() postData: PostData | undefined;
   //next got to post.comp.html and ues {{}}
+  creatorName: string | undefined;
+  creatorDescription :string | undefined;
+
+  firestore = new FirebaseTSFirestore();
   constructor() { }
 
   ngOnInit(): void {
+    this.getCreatorInfo();
   }
 
+  getCreatorInfo(){  // to retreive the info of the user made the post
+    this.firestore.getDocument( // as in DB thers a collection Users and data is saved as documne in it
+      {
+        path: ["Users", this.postData?.creatorId || ''],
+        onComplete: result => {
+          let userDocument = result.data();
+          this.creatorName = userDocument?.['publicName'];
+          this.creatorDescription = userDocument?.['description'];
+
+        }
+
+
+      }
+    );
+  }
 }
